9ad236275b553b299914303e11f948108a5e40ef
leidy-m <7032681@gmail.com>
2025-04-03 19:41:01 -0500

    dist files

--- estructuras/vue.config.js
+++ estructuras/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/leidy-m.github.io/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/leidy-m.github.io/estructuras/" : "/",
   };
