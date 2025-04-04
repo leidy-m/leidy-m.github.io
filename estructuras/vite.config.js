62dd7dd32dd93dc064ff1f6cc2e9ac2b81f2b285
leidy-m <7032681@gmail.com>
2025-04-03 19:47:53 -0500

    Base on DefineConfig

--- estructuras/vite.config.js
+++ estructuras/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/leidy-m.github.io/estructuras/",
   plugins: [
     vue(),
     vueDevTools(),
