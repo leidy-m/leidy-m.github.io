1684eed85fc56de46fca20698d98595d710d8f85
leidy-m <7032681@gmail.com>
2025-04-03 19:58:22 -0500

    Base

--- estructuras/vite.config.js
+++ estructuras/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/leidy-m.github.io/estructuras/",
+  base: "/leidy-m.github.io/",
   plugins: [
     vue(),
     vueDevTools(),
--- estructuras/vue.config.js
+++ estructuras/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/leidy-m.github.io/estructuras/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/leidy-m.github.io/" : "/",
   };
